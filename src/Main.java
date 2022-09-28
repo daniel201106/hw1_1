a72e5896a244b46ca70e7cd644da4d6d06540ef1
unknown <danielsydykov757@gmail.com>
2022-09-28 16:25:40 +0600

    first project

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+﻿
+public class Main {
+
+    public static void main(String[] args) {
+        String gas;
+        final int NUM = 2;
+        String word = "car";
+        gas = NUM + word ;
+
+        System.out.println(gas);
+
+        if(NUM < 0 ) {
+            System.out.println("Вы сохранили отрицательное число");
+        } else if (NUM > 0) {
+            System.out.println("Вы сохранили положительное число");
+        }else {
+            System.out.println("Вы сохранили ноль");
+        }
+
+    }
+}
